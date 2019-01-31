79452a0d7a5993981de7d18272ed3521838fee52
NovaStriker <johnaldoc@hotmail.com>
2019-01-30 23:29:41 -0500

    cambio matix

---
+++ Mochila/src/mochila/Matrix.java
@@ -0,0 +1,55 @@
+package Mochila;
+
+public class Matrix {
+    
+    private int Matrix[][];
+    private int Fila;
+    private int Columna;
+
+    public int[][] getMatrix() {
+        return Matrix;
+    }
+
+    public void setMatrix(int[][] Matrix) {
+        this.Matrix = Matrix;
+    }
+    
+    public int getFila() {
+        return Fila;
+    }
+
+    public void setFila(int Fila) {
+        this.Fila = Fila;
+    }
+    
+    public int getColumna() {
+        return Columna;
+    }
+
+    public void setColumna(int Columna) {
+        this.Columna = Columna;
+    }
+    
+    public Matrix(int nuevaColumna, int nuevaFila) {
+        Matrix = new int[nuevaColumna][nuevaFila];
+        Columna = nuevaColumna;
+        Fila = nuevaFila;
+        
+        for (int i = 0; i < Columna; i++) {
+            for (int j = 0; j < Fila; j++) {
+                Matrix[i][j] = 0;
+            }
+            System.out.println();
+        }
+    }
+
+    public void ShowMatrix() {
+        for (int i = 0; i < Columna; i++) {
+            for (int j = 0; j < Fila; j++) {
+                System.out.printf("%5d", Matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
+    
+}
